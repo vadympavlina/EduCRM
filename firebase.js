5e6ed0f81a480cb96d28455b6133f712be607a6b
Vadym Pavlina <42926781+vadympavlina@users.noreply.github.com>
2026-04-21 13:27:28 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,14 @@
 //  console.firebase.google.com → Project Settings → Your Apps
 // ============================================================
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyAjA4rxjvk0_2sILu11Bfc-UBiJ5LuDjJI",
+    authDomain: "educrm-85756.firebaseapp.com",
+    databaseURL: "https://educrm-85756-default-rtdb.firebaseio.com",
+    projectId: "educrm-85756",
+    messagingSenderId: "248243910161",
+    appId: "1:248243910161:web:43c4fc223c3754c0ffe3b0"
+  };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
